99f1875461620e08d4f18eb554abba32a7ef0834
jalb850420 <70179616+jalb850420@users.noreply.github.com>
2020-09-27 21:38:55 -0500

    Actualizacion index

--- web/index.php
+++ web/index.php
@@ -1,23 +1,46 @@
-<?php
-require('../vendor/autoload.php');
-$app = new Silex\Application();
-$app['debug'] = true;
-// Register the monolog logging service
-$app->register(new Silex\Provider\MonologServiceProvider(), array(
-  'monolog.logfile' => 'php://stderr',
+<? php
+
+use  Symfony \ Component \ HttpFoundation \ Request ;
+
+require ( '../vendor/autoload.php' );
+
+$ aplicación = nueva  Silex \ Aplicación ();
+$ aplicación [ 'depuración' ] = verdadero ;
+
+// Registrar el servicio de registro de monólogos
+$ aplicación -> registro ( nuevo  Silex \ Provider \ MonologServiceProvider (), array (
+  'monolog.logfile' => 'php: // stderr' ,
 ));
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
+
+// Registrar la representación de la vista
+$ aplicación -> registrar ( nuevo  Silex \ Provider \ TwigServiceProvider (), array (
+    'twig.path' => __DIR__. '/ vistas' ,
 ));
-// Our web handlers
-$app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
+
+// Nuestros manipuladores web
+
+$ aplicación -> get ( '/' , function () use ( $ app ) {
+  $ app [ 'monolog' ] -> addDebug ( 'salida de registro.' );
+  return  $ aplicación [ 'ramita' ] -> render ( 'index.twig' );
 });
 
-$app->get('/Consulta', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('Consulta.twig');
+
+// Ruta de demostración, para validar que se recibe (n) dato (s) y se responde con este mismo
+$ app -> post ( '/ enviarDato' , function ( Request  $ request ) use ( $ app ) {
+   devolver  $ solicitud ;
 });
-$app->run();
+
+
+// Ruta de demostración, se recibe (n) dato (s) y se manipulan
+$ app -> post ( '/ modificarDato' , function ( Request  $ request ) use ( $ app ) {
+   	$ nombre = $ solicitud -> get ( 'nombre' );
+	$ respuesta = "Hola" . $ nombre ;
+   	return  $ respuesta ;
+});
+
+// Ruta de demostración, se recibe (n) dato (s) y se manipulan
+$ app -> post ( '/ postArduino' , function ( Request  $ request ) use ( $ app ) {
+   	devuelve  "OK" ;
+});
+
+$ aplicación -> ejecutar ();
