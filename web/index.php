cc60b5265db6a6c7ded523d4ed5e8be6866df6db
jalb850420 <70179616+jalb850420@users.noreply.github.com>
2020-09-30 15:36:24 -0500

    Actualizacion de mensaje POST

--- web/index.php
+++ web/index.php
@@ -39,8 +39,8 @@ $app->post('/modificarDato', function (Request $request) use ($app) {
 });
 
 //Ruta de demostración, se recibe(n) dato(s) y se manipulan
-$app->post('/postArduino', function (Request $request) use ($app) {
-   	return "OK";
+$app->post('/postPruebaJALB', function (Request $request) use ($app) {
+   	return "Post SmartHome 2.0";
 });
 
 $app->run();
